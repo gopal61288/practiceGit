efce0c6031e1bb7833762f15f85c99de06c0c8b4
gopal61288 <pedabicar@gmail.com>
2022-04-06 11:27:15 +0530

    array branch #12

--- javaScript/arrays/array23.js
+++ javaScript/arrays/array23.js
@@ -9,3 +9,14 @@ arr24.forEach((item, index) => {
     console.log(item);
 })
 console.log(arr24);         // Inspect why this didnt work...
+
+let arr25 = [
+    {name: "gopal", club: "Real Madrid", country: "Spain"},
+    {name: "vijay", club: "Barca", country: "Spain"},
+    {name: "mahesh", club: "Arsenal", country: "England"}
+];
+
+for(let i=0; i<arr25.length; i++) {
+    console.log(`${arr25[i].name} likes ${arr25[i].club} from ${arr25[i].country}`);
+}
+
