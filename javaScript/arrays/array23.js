62f51e8eabe4809eb7f79713c9ae8e2fa434be0d
gopal61288 <pedabicar@gmail.com>
2022-04-06 11:14:00 +0530

    some changes in arrays branch

--- javaScript/arrays/array23.js
+++ javaScript/arrays/array23.js
@@ -1,4 +1,11 @@
 const arr23 = ['gopal', 1, true]
 arr23.forEach((item, index) => {
     console.log(`element at ${index} position is ${item}`);
+});
+
+let arr24 = ['Alaba', 'Modric', 'Kroos', 'Benzema']
+arr24.forEach((item, index) => {
+    item = item + '_RM';
+    console.log(item);
 })
+console.log(arr24);         // Inspect why this didnt work...
