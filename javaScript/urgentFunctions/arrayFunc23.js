d3cd2229440207e810ef27b990763d764f96f819
gopal61288 <pedabicar@gmail.com>
2022-04-06 11:06:37 +0530

    curried func from urgentFunctions branch

--- javaScript/urgentFunctions/arrayFunc23.js
+++ javaScript/urgentFunctions/arrayFunc23.js
@@ -1,3 +1,9 @@
 const addNum23 = (num1, num2) => num1+num2;
-const totalley = addNum23(6,2);
-console.log(totalley);
+const totalley23 = addNum23(6,2);
+console.log(totalley23);
+
+const addNum24 = num1 => num2 => num1+num2;
+const totalley24 = addNum24(6)(7);
+// const totalley24 = addNum24(6, 67);  // use this line... see how totalley24 changes color from blue to yellow
+                                            // it seems, blue is number/string/something... yellow is function (like addNum23, addNum24)
+console.log(totalley24);
